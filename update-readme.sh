dab97375ad9886a2cf8e3105c31f78c3a6dfa1c6
sagesteppe <reedbenkendorf27@gmail.com>
2026-01-04 20:18:39 -0600

    test pagedown

--- update-readme.sh
+++ update-readme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Render README.rmd to README.md
-Rscript -e 'rmarkdown::render("README.rmd", encoding="UTF8")'
+Rscript -e 'rmarkdown::render("README.Rmd", encoding="UTF8")'
 
 echo "README updated successfully!"
